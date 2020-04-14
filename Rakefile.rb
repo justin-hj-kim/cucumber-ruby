ffbc8343379936a51bfb322313411e76a8687010
Justin Kim <Justin.Kim@smartbear.com>
2020-04-14 17:04:02 -0400

    'rake111333'

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,5 +3,5 @@ require 'cucumber'
 require 'cucumber/rake/task'
 
 Cucumber::Rake::Task.new(:features) do |t|
-  t.cucumber_opts = "bundle exec --format json --out results.json" # Any valid command line option can go here.
+  t.cucumber_opts = "--format json --out results.json" # Any valid command line option can go here.
 end
