b85b7d41c271e049f8c3b3524dfea70f5f9185ee
Justin Kim <Justin.Kim@smartbear.com>
2020-04-15 09:14:57 -0400

    gems

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'cucumber'
+require 'rake'
+require 'bundler'
+require 'selenium-webdriver'
 require 'cucumber/rake/task'
+
 
 Cucumber::Rake::Task.new(:features) do |t|
   t.cucumber_opts = "--format json --out results.json" # Any valid command line option can go here.
