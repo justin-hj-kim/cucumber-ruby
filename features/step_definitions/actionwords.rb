9b88ded662268048e4a4cbdd5595b7d24a6d8823
Justin Kim <Justin.Kim@smartbear.com>
2020-04-27 10:22:07 -0400

    cuke reports

--- features/step_definitions/actionwords.rb
+++ features/step_definitions/actionwords.rb
@@ -83,3 +83,6 @@ end
 #hiptest-publisher -c hiptest-publisher.conf --token=207233293921290597634285569888014368421957412936171795605 --only=features,step_definitions --test-run-id=389046
 #cucumber --format json -o results.json
 #hiptest-publisher -c hiptest-publisher.conf --token=207233293921290597634285569888014368421957412936171795605  --push-format="cucumber-json" --push="results.json" --test-run-id=389046
+
+#go to https://reports.cucumber.io/
+#cucumber --format message --out https://b1f2yk58d8.execute-api.eu-west-3.amazonaws.com/api/repository/5985db57-cb09-480d-8417-c797706ff8e4
